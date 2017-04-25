155a684096def5e27a847eafb08dc71200c23cc2
jakeniemiec <jake.j.niemiec@gmail.com>
2017-04-25 10:28:49 -0500

    fix: lint and .MD update

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 # Fuzzy Finder for Chrome
 
-Type `ff` in your address bar followed by your keywords to fuzzy search your tabs and history.
+Type `l` in your address bar followed by your keywords to fuzzy search your tabs and history.
 
 **Note:** To select the first match, you can press Enter without pressing down or selecting any of the results.
-
-<p align="center"> <a href="#"><img src="screenshot-1.3.png"/></a> </p>
 
 ## Usage
 
@@ -12,7 +10,7 @@ Let's say you have 100 tabs open, in multiple windows.
 You want to find a youtube tab of a talk you were watching a few hours ago.
 Instead of going through all your tabs one by one, you could just type (in your address bar):
 
-    ff talk ytube
+    l talk ytube
 
 and you will see a list of all the tabs that match the phrase "ytube" and "talk" in any order.
 
@@ -20,17 +18,12 @@ Matching is fuzzy, so "ytube" will match "youtube".
 
 If no matching open tab is found, the extension will show matching items in your history.
 
-## Install
-
-Download [chrome-ff-1.4.crx](https://github.com/siadat/chrome-ff/releases/download/1.4/chrome-ff.crx)
-and drop it in your `chrome://extensions/` page.
-
 ## Contributing
 
 - Fork and submit a pull request
-- [Send a feature request](https://github.com/siadat/chrome-ff/issues/new)
-- [Report a problem](https://github.com/siadat/chrome-ff/issues/new)
+- [Send a feature request](https://github.com/jakeNiemiec/FuzzyChrome/issues/new)
+- [Report a problem](https://github.com/jakeNiemiec/FuzzyChrome/issues/new)
 
 ## License
 
-This extension is released under the [MIT License](http://www.opensource.org/licenses/MIT).
+This extension is Copyright (c) 2017 Sina Siadat (the original author). It was forked under the [MIT License](http://www.opensource.org/licenses/MIT) from [chrome-ff](https://github.com/siadat/chrome-ff).
--- background.js
+++ background.js
@@ -9,95 +9,113 @@ var ffCurrentWindowId;
 var ffTabsOnStart = [];
 
 function ffEscapeRegExp(str) {
-  return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+  return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&');
 }
 
 function ffActivateTag(tab) {
-  if(tab.url) {
-    chrome.tabs.update({url: tab.url});
+  if (tab.url) {
+    chrome.tabs.update({ url: tab.url });
   }
-  if(tab.tabId) {
-    chrome.tabs.update(tab.tabId, {active: true});
-    if(FF_MOVE_TAB_TO_FIRST) {
-      chrome.tabs.move(tab.tabId, {index: 0});
+  if (tab.tabId) {
+    chrome.tabs.update(tab.tabId, { active: true });
+    if (FF_MOVE_TAB_TO_FIRST) {
+      chrome.tabs.move(tab.tabId, { index: 0 });
     }
   }
-  if(tab.windowId) {
-    chrome.windows.update(tab.windowId, {focused: true});
+  if (tab.windowId) {
+    chrome.windows.update(tab.windowId, { focused: true });
   }
 }
 
 function ffEscapeHtml(unsafe) {
-  return unsafe.replace(/&/g, "&amp;")
-               .replace(/</g, "&lt;")
-               .replace(/>/g, "&gt;")
-               .replace(/"/g, "&quot;")
-               .replace(/'/g, "&#039;");
+  return unsafe.replace(/&/g, '&amp;')
+               .replace(/</g, '&lt;')
+               .replace(/>/g, '&gt;')
+               .replace(/"/g, '&quot;')
+               .replace(/'/g, '&#039;');
 }
 
 function ffGetHostname(url) {
-  var a  = document.createElement('a');
+  var a = document.createElement('a');
   a.href = url;
   return a.hostname;
 }
 
 function ffHighlightText(text, words) {
   words
-  .map(function(word) { return ffRegexpExactHl(word); })
-  .forEach(function(word) {
-    if(text.match(word)) {
-      text = text.replace(word, "\0$1\1")
-    }
-  });
-
+    .map(function (word) { return ffRegexpExactHl(word); })
+    .forEach(function (word) {
+      if (text.match(word)) {
+        text = text.replace(word, '\0$1\1');
+      }
+    });
+  
   words
-  .map(function(word) { return ffRegexpFuzzy(word); })
-  .forEach(function(word) {
-    if(text.match(word)) {
-      text = text.replace(word, function(m) {
-        return "\0" + m + "\1";
-      });
-    }
-  });
-  return ffEscapeHtml(text).replace(new RegExp("\0", "g"), "<match>")
-                           .replace(new RegExp("\1", "g"), "</match>");
+    .map(function (word) { return ffRegexpFuzzy(word); })
+    .forEach(function (word) {
+      if (text.match(word)) {
+        text = text.replace(word, function (m) {
+          return '\0' + m + '\1';
+        });
+      }
+    });
+  return ffEscapeHtml(text).replace(new RegExp('\0', 'g'), '<match>')
+                           .replace(new RegExp('\1', 'g'), '</match>');
 }
 
 function ffCalculateScoreWords(tab, words) {
   var score = 0;
   var hostname = ffGetHostname(tab.url);
-
-  var found_words = words.map(function() { return false; })
-
+  
+  var found_words = words.map(function () { return false; });
+  
   words
-  .map(function(word) { return ffRegexpExact(word); })
-  .forEach(function(word, i) {
-    if(hostname.match(word)) { score += 100; found_words[i] = true; }
-    if(tab.title.match(word)) { score += 90; found_words[i] = true; }
-    if(tab.url.match(word)) { score += 100; found_words[i] = true; }
-  });
-
+    .map(function (word) { return ffRegexpExact(word); })
+    .forEach(function (word, i) {
+      if (hostname.match(word)) {
+        score += 100;
+        found_words[i] = true;
+      }
+      if (tab.title.match(word)) {
+        score += 90;
+        found_words[i] = true;
+      }
+      if (tab.url.match(word)) {
+        score += 100;
+        found_words[i] = true;
+      }
+    });
+  
   words
-  .map(function(word) { return ffRegexpFuzzy(word); })
-  .forEach(function(word, i) {
-    if(found_words[i]) { return; }
-    if(hostname.match(word)) { score += 20; found_words[i] = true; }
-    if(tab.title.match(word)) { score += 20; found_words[i] = true; }
-    if(tab.url.match(word)) { score += 30; found_words[i] = true; }
-  });
-
-  if(tab.visitCount) {
+    .map(function (word) { return ffRegexpFuzzy(word); })
+    .forEach(function (word, i) {
+      if (found_words[i]) { return; }
+      if (hostname.match(word)) {
+        score += 20;
+        found_words[i] = true;
+      }
+      if (tab.title.match(word)) {
+        score += 20;
+        found_words[i] = true;
+      }
+      if (tab.url.match(word)) {
+        score += 30;
+        found_words[i] = true;
+      }
+    });
+  
+  if (tab.visitCount) {
     score += tab.visitCount * 10;
   }
-
-  if(tab.lastVisitTime) {
+  
+  if (tab.lastVisitTime) {
     score += tab.lastVisitTime / (new Date().getTime());
   }
-
-  if(found_words.filter(function(x) { return x; }).length !== words.length) { score = 0; }
-  if(score > 0 && tab.pinned) { score += 1000; }
-
-  if(FF_DEBUGGING && score > 0) { console.debug("matching tab", tab.lastVisitTime && "History" || "Opened", "id:"+ tab.id, "score:"+ score, hostname, tab); }
+  
+  if (found_words.filter(function (x) { return x; }).length !== words.length) { score = 0; }
+  if (score > 0 && tab.pinned) { score += 1000; }
+  
+  if (FF_DEBUGGING && score > 0) { console.debug('matching tab', tab.lastVisitTime && 'History' || 'Opened', 'id:' + tab.id, 'score:' + score, hostname, tab); }
   return score;
 }
 
@@ -106,54 +124,57 @@ function ffRegexpExact(word) {
 }
 
 function ffRegexpExactHl(word) {
-  return new RegExp("(" + ffEscapeRegExp(word) + ")", 'ig');
+  return new RegExp('(' + ffEscapeRegExp(word) + ')', 'ig');
 }
 
 function ffRegexpFuzzy(word) {
-  return new RegExp(word.split('').map(function(ch) { return ffEscapeRegExp(ch); }).join('.{0,10}?'), 'i');
+  return new RegExp(word.split('').map(function (ch) { return ffEscapeRegExp(ch); }).join('.{0,10}?'), 'i');
 }
 
 function ffPrepareTab(tab, words) {
-  var content = JSON.stringify({tabId: tab.id, windowId: tab.windowId});
-  var desc =  "🌐<url>" + ffHighlightText(tab.url, words) + "</url> 📄" + ffHighlightText(tab.title, words);
-
+  var content = JSON.stringify({ tabId: tab.id, windowId: tab.windowId });
+  var desc = '🌐<url>' + ffHighlightText(tab.url, words) + '</url> 📄' + ffHighlightText(tab.title, words);
+  
   // if(FF_DEBUGGING) {
   //   desc = "score:" + tab.score + " - " + desc;
   // }
-
-  if(tab.status && tab.status !== "complete") {
-    desc = "[" + tab.status + "] " + desc;
+  
+  if (tab.status && tab.status !== 'complete') {
+    desc = '[' + tab.status + '] ' + desc;
   }
-
-  if(tab.incognito) {
-    desc = "<url>[🔒]</url> " + desc;
+  
+  if (tab.incognito) {
+    desc = '<url>[🔒]</url> ' + desc;
   }
-
-  if(tab.pinned) {
-    desc = "<url>[📌]</url> " + desc;
+  
+  if (tab.pinned) {
+    desc = '<url>[📌]</url> ' + desc;
   }
-
-  if(tab.audible) {
-    desc = "<url>[🔊]</url> " + desc;
+  
+  if (tab.audible) {
+    desc = '<url>[🔊]</url> ' + desc;
   }
-
-  if(tab.lastVisitTime) {
-    content = JSON.stringify({url: tab.url});
-    desc = "<url>[🕑]</url> " + desc;
+  
+  if (tab.lastVisitTime) {
+    content = JSON.stringify({ url: tab.url });
+    desc = '<url>[🕑]</url> ' + desc;
   }
-
-  return {content: content, description: desc};
+  
+  return { content: content, description: desc };
 }
 
 function ffFilter(tabs, words) {
-  return tabs.map(function(tab) { tab.score = ffCalculateScoreWords(tab, words); return tab; })
-             .filter(function(tab) { return tab.score >= 10 && tab.title.length > 0; })
-             .sort(function(tab1, tab2) {
-                if(tab1.score < tab2.score) return 1;
-                if(tab1.score > tab2.score) return -1;
-                return 0;
-              })
-             ;
+  return tabs.map(function (tab) {
+               tab.score = ffCalculateScoreWords(tab, words);
+               return tab;
+             })
+             .filter(function (tab) { return tab.score >= 10 && tab.title.length > 0; })
+             .sort(function (tab1, tab2) {
+               if (tab1.score < tab2.score) return 1;
+               if (tab1.score > tab2.score) return -1;
+               return 0;
+             })
+    ;
 }
 
 function ffConcat(tabs1, tabs2) {
@@ -169,39 +190,39 @@ function ffTabsWithout(whiteTabs, blackTabs) {
 function ffReorderTabs(tabs) {
   var windows = {};
   /*
-  *  move ones for all (doesn't work correctly):
-  *
-  *  chrome.tabs.move(tabs.map(function(tab) { return tab.id; }), {index: 0});
-  */
-
+   *  move ones for all (doesn't work correctly):
+   *
+   *  chrome.tabs.move(tabs.map(function(tab) { return tab.id; }), {index: 0});
+   */
+  
   /*
-  *  move once for all tabs in each window (doesn't work correctly):
-  *
-  *  tabs.forEach(function(tab) {
-  *    if(!windows[tab.windowId]) {
-  *      windows[tab.windowId] = [];
-  *    }
-  *    windows[tab.windowId].push(tab);
-  *  });
-  *  for(var windowId in windows) {
-  *    console.log("WINDOW", windowId, windows[windowId]);
-  *    chrome.tabs.move(windows[windowId].map(function(tab) { return tab.id; }), {index: 0, windowId: parseInt(windowId)});
-  *  }
-  */
-
-  if(ffTabsOnStart.length === 0) {
-    ffTabsOnStart = tabs.map(function(tab) { return {id: tab.id, index: tab.index}; });
+   *  move once for all tabs in each window (doesn't work correctly):
+   *
+   *  tabs.forEach(function(tab) {
+   *    if(!windows[tab.windowId]) {
+   *      windows[tab.windowId] = [];
+   *    }
+   *    windows[tab.windowId].push(tab);
+   *  });
+   *  for(var windowId in windows) {
+   *    console.log("WINDOW", windowId, windows[windowId]);
+   *    chrome.tabs.move(windows[windowId].map(function(tab) { return tab.id; }), {index: 0, windowId: parseInt(windowId)});
+   *  }
+   */
+  
+  if (ffTabsOnStart.length === 0) {
+    ffTabsOnStart = tabs.map(function (tab) { return { id: tab.id, index: tab.index }; });
   }
-
-  tabs.forEach(function(tab, i) {
-    if(FF_MOVE_TAB_TO_FIRST_TO_CURRENT_WINDOW && ffCurrentWindowId) {
+  
+  tabs.forEach(function (tab, i) {
+    if (FF_MOVE_TAB_TO_FIRST_TO_CURRENT_WINDOW && ffCurrentWindowId) {
       // move one by one to current window
-      chrome.tabs.move(tab.id, {index: i, windowId: ffCurrentWindowId});
+      chrome.tabs.move(tab.id, { index: i, windowId: ffCurrentWindowId });
     } else {
       // move one by one
-      if(!windows[tab.windowId]) { windows[tab.windowId] = []; }
+      if (!windows[tab.windowId]) { windows[tab.windowId] = []; }
       windows[tab.windowId].push(true);
-      chrome.tabs.move(tab.id, {index: windows[tab.windowId].length - 1});
+      chrome.tabs.move(tab.id, { index: windows[tab.windowId].length - 1 });
     }
   });
 }
@@ -209,71 +230,75 @@ function ffReorderTabs(tabs) {
 function ffSearchFor(text) {
   text = text.trim();
   var words = text.split(/\s+/);
-
-  var words_exact_hl = text.split(/\s+/).map(function(word) {
+  
+  var words_exact_hl = text.split(/\s+/).map(function (word) {
     return ffRegexpExactHl(word);
   });
-
-  return new Promise(function(resolve, reject) {
-    chrome.tabs.query({}, function(array_of_tabs) {
+  
+  return new Promise(function (resolve, reject) {
+    chrome.tabs.query({}, function (array_of_tabs) {
       var matching_tabs = ffFilter(array_of_tabs, words);
-
-      if(FF_MOVE_TAB_TO_FIRST) {
+      
+      if (FF_MOVE_TAB_TO_FIRST) {
         ffReorderTabs(matching_tabs.slice(0, 200));
       }
-
+      
       matching_tabs = matching_tabs.slice(0, FF_MAX_SUGGESTIONS);
-      if(FF_INCLUDE_HISTORY && matching_tabs.length < FF_MAX_SUGGESTIONS) {
-        chrome.history.search({text: "", maxResults: 100, startTime: new Date().getTime() - 30 * 24 * 3600 * 1000}, function(array_of_history_items) {
+      if (FF_INCLUDE_HISTORY && matching_tabs.length < FF_MAX_SUGGESTIONS) {
+        chrome.history.search({
+                                text: '',
+                                maxResults: 100,
+                                startTime: new Date().getTime() - 30 * 24 * 3600 * 1000
+                              }, function (array_of_history_items) {
           var matching_histories = ffFilter(array_of_history_items, words);
-          resolve(ffConcat(matching_tabs.slice(0, FF_MAX_SUGGESTIONS), matching_histories).map(function(tab) { return ffPrepareTab(tab, words); } ));
+          resolve(ffConcat(matching_tabs.slice(0, FF_MAX_SUGGESTIONS), matching_histories).map(function (tab) { return ffPrepareTab(tab, words); }));
         });
         return;
       }
-      resolve(matching_tabs.map(function(tab) { return ffPrepareTab(tab, words); }));
+      resolve(matching_tabs.map(function (tab) { return ffPrepareTab(tab, words); }));
     });
   });
 }
 
 chrome.windows.onFocusChanged.addListener(
-  function(windowId) {
+  function (windowId) {
     ffCurrentWindowId = windowId;
   }
 );
 
 chrome.omnibox.onInputChanged.addListener(
-  function(text, suggest) {
-    if(FF_DEBUGGING) { console.debug("input changed:", text); }
-    ffSearchFor(text).then(function(suggestions) { suggest(suggestions); });
+  function (text, suggest) {
+    if (FF_DEBUGGING) { console.debug('input changed:', text); }
+    ffSearchFor(text).then(function (suggestions) { suggest(suggestions); });
   }
 );
 
 chrome.omnibox.onInputCancelled.addListener(
   // TODO this is called even when user presses UP/Down arrows.
-  function() {
+  function () {
     // revert tab order (one by one)
-    ffTabsOnStart.sort(function(tab1, tab2) {
-      if(tab1.index < tab2.index) return 1;
-      if(tab1.index > tab2.index) return -1;
+    ffTabsOnStart.sort(function (tab1, tab2) {
+      if (tab1.index < tab2.index) return 1;
+      if (tab1.index > tab2.index) return -1;
       return 0;
-    }).forEach(function(tab, i) {
-      chrome.tabs.move(tab.id, {index: tab.index});
+    }).forEach(function (tab, i) {
+      chrome.tabs.move(tab.id, { index: tab.index });
     });
     ffTabsOnStart = [];
   }
 );
 
 chrome.omnibox.onInputEntered.addListener(
-  function(text) {
-    if(FF_DEBUGGING) {
-      console.debug("entered:", text);
-      console.debug("history:", ffHistory);
+  function (text) {
+    if (FF_DEBUGGING) {
+      console.debug('entered:', text);
+      console.debug('history:', ffHistory);
     }
-
+    
     var selected = {};
-
-    if(text.length === 0) {
-      if(ffHistory.length >= 2) {
+    
+    if (text.length === 0) {
+      if (ffHistory.length >= 2) {
         selected = ffHistory[ffHistory.length - 2];
       } else {
         return;
@@ -281,11 +306,11 @@ chrome.omnibox.onInputEntered.addListener(
     } else {
       try {
         selected = JSON.parse(text);
-      } catch(e) {
+      } catch (e) {
         // User probably typed something but selected the first default option,
         // i.e., "Run ff command: query"
-        ffSearchFor(text).then(function(suggestions) {
-          if(suggestions.length === 0) { return; }
+        ffSearchFor(text).then(function (suggestions) {
+          if (suggestions.length === 0) { return; }
           var selected = JSON.parse(suggestions[0].content);
           ffHistory.push(selected);
           ffActivateTag(selected);
@@ -293,7 +318,7 @@ chrome.omnibox.onInputEntered.addListener(
         return;
       }
     }
-
+    
     ffHistory.push(selected);
     ffActivateTag(selected);
   }
